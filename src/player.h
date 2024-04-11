79eda27f8ae5e306746d43d37be16fee22a1a63d
tinlizard <38041074+tinlizard@users.noreply.github.com>
2024-04-11 19:02:21 +0200

    Create player.h

---
+++ src/player.h
@@ -0,0 +1,32 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include "raylib.h"
+#include <stdlib.h>
+#include <stdio.h>
+#define TEXTURE_LEN 480
+#define IDLE_TEXTURE_LEN 1200
+#define NUM_FIGHT_SPRITES 4
+#define NUM_WALK_SPRITES 10
+#define NUM_IDLE_SPRITES 10
+
+typedef struct {
+   int x, y, textureWidth, textureHeight, idleIndex, 
+   walkIndex, fightIndex, walkTextureWidth, 
+   idleTextureWidth,walkTexureHeight;
+   Texture2D idleTexture, fightTexture, walkTexture;
+   Rectangle srcFightRects[NUM_FIGHT_SPRITES];
+   Rectangle srcWalkRects[NUM_WALK_SPRITES];
+   Rectangle srcIdleRects[NUM_IDLE_SPRITES];
+   //Rectangle idleRect; 
+
+   Rectangle destRect;
+   Rectangle destWalkRect;
+   bool isIdling, isMoving, isFighting, facingRight;
+} Player;
+
+Player* initPlayer();
+void animateFightPlayer(Player* player, int* fightIndex, float* currentTime);
+void animateWalkPlayer(Player* player, int* walkIndex, float* currentTime);
+void animateIdlePlayer(Player* player, int* idleIndex, float* currentTime);
+
+#endif
