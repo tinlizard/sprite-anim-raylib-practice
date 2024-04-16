bc7d04481205906352ba3a75733d4ba77952393d
tinlizard <38041074+tinlizard@users.noreply.github.com>
2024-04-16 13:03:08 +0200

    Update platform.c

--- src/platform.c
+++ src/platform.c
@@ -1,23 +1,25 @@
 #include "platform.h"
 #include "player.h"
 
-Platform* initPlatform(){
+Platform* initPlatform(int x, int y, int width, int height){
     Platform* platform = malloc(sizeof(Platform));
 
-    platform->x = 40; 
-    platform->y = 750;
-    platform->width = 200;
-    platform->height = 20;
+    platform->x = x; 
+    platform->y = y;
+    platform->width = width;
+    platform->height = height;
     platform->isCollider = true;
     platform->collisionRect = (Rectangle) {platform->x,platform->y,platform->width,platform->height};
-    platform->texture = LoadTexture("/home/ben/Documents/Free Pixel Art Forest/Preview/background.png");
-
+    platform->texture = LoadTexture("/home/ben/Documents/FreeCuteTileset/ExampleTiles.png");
+    platform->srcRect = (Rectangle) {20,0,80,96};
+    platform->destRect = (Rectangle) {platform->x,platform->y,platform->width,platform->height};
+    
     return platform;
 }
 
 //debugging function
 bool isCollidingWithPlayer(Player* player, Platform* platform){
-   if(CheckCollisionRecs(player->destRect,platform->collisionRect))
+   if(CheckCollisionRecs(player->collisionRect,platform->collisionRect))
 	return true;	
    else 
     	return false;
